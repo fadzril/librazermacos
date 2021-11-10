c2e9165a29973c8acf5efda574798f757e8adc65
Fadzril Muhamad <fadzril@jomcode.com>
2021-11-10 10:26:04 +0800

    Added support for BlackWidow V3 Mini

--- src/sample_cli.c
+++ src/sample_cli.c
@@ -18,6 +18,21 @@ int main(int argc, const char * argv[]) {
             printf("Found blackwidow v3 pro keyboard (wired)\n");
             razer_attr_write_mode_wave(device.usbDevice, "1", 0, 0x90);
         }
+        if (device.productId == 0x0271)
+        {
+            printf("Found blackwidow v3 mini keyboard (dongle)\n");
+            razer_attr_write_mode_wave(device.usbDevice, "1", 0, 0x90);
+        }
+        if (device.productId == 0x0258)
+        {
+            printf("Found blackwidow v3 mini keyboard (usb)\n");
+            razer_attr_write_mode_wave(device.usbDevice, "1", 0, 0x90);
+        }
+        if (device.productId == 0x0088 || device.productId == 0x0086)
+        {
+            printf("Found basilisk ultimate mouse receiver (dongle)\n");
+            razer_attr_write_mode_wave(device.usbDevice, "1", 0, 0x90);
+        }
     }
 
     closeAllRazerDevices(allDevices);
